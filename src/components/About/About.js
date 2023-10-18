76d60708e8eeb0b8678ecdf1a8cf1e18b8594162
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-18 15:43:55 +0300

    implement new design for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,39 +5,45 @@ export default function About() {
         <section className={style.about}>
             <section className={style.cardSection}>
                 <section className={style.card}>
-                    <img className={style.aboutImg} src={"../../../1.png"} alt="" />
                     <h2>Who are we?</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Perspiciatis assumenda minus vel laborum possimus.
-                        
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                        tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    </p>
+                    <p>
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                        aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor.
                     </p>
                 </section>
 
                 <section className={style.card}>
-                    <img className={style.aboutImg} src={"../../../2.png"} alt="" />
                     <h2>What do we want?</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Perspiciatis assumenda minus vel laborum possimus.
-                        
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                        tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    </p>
+                    <p>
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                        aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor.
                     </p>
                 </section>
 
                 <section className={style.card}>
-                    <img className={style.aboutImg} src={"../../../3.png"} alt="" />
                     <h2>How it works?</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Perspiciatis assumenda minus vel laborum possimus.
-                        
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                        tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    </p>
+                    <p>
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                        aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor.
                     </p>
                 </section>
             </section>
             <section className={style.lastSection}>
-                <h2>Say hi!</h2>
+                <h2>{"Say hi! :)"}</h2>
                 <p>center@trivia.com</p>
-                <p>+359 877 8777</p>
+                <p>+359 877 877 877</p>
             </section>
         </section>
     );
