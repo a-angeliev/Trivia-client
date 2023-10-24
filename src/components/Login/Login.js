613e44b276b64ddb4770a1ff28a0a13556e0f5bf
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-24 13:45:59 +0300

    implement new design for login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,59 +1,66 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import style from "./login.module.css";
+
 import { AuthContext } from "../../context/authContext";
 import * as authService from "./../../service/authService";
 
+import style from "./login.module.css";
+
 export default function Login() {
     const { userLogin } = useContext(AuthContext);
+
     const navigate = useNavigate();
 
     const onSubmit = (e) => {
         e.preventDefault();
-        let {email, password} = Object.fromEntries(new FormData(e.target))
-        authService.login(email , password).then(authData=> {
-            if(authData.token){
-                userLogin(authData);
-                navigate('/');
-            }else{
-                alert(JSON.stringify(authData['message']));
-            }
-            
-        }).catch((err) => {
-            console.log(err);
-            navigate('/');
-        });
-
+        let { email, password } = Object.fromEntries(new FormData(e.target));
+        authService
+            .login(email, password)
+            .then((authData) => {
+                if (authData.token) {
+                    userLogin(authData);
+                    navigate("/");
+                } else {
+                    alert(JSON.stringify(authData["message"]));
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                alert(err);
+                navigate("/");
+            });
     };
     return (
         <section className={style.loginSection}>
             <form onSubmit={onSubmit} className={style.loginForm}>
-                <label className={style.loginLables} htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className={style.loginInputs}
-                    id="email"
-                    placeholder="Ivan@gmail.com"
-                    name="email"
-                ></input>
-
-                <label className={style.loginLables} htmlFor="password">
-                    Password
-                </label>
-                <input
-                    className={style.loginInputs}
-                    id="password"
-                    placeholder="***********"
-                    name="password"
-                    type="password"
-                ></input>
-
+                <h1 className={style.loginTitle}>Sign in</h1>
+                <div className={style.inputDiv}>
+                    <label className={style.loginLabels} htmlFor='email'>
+                        email address
+                    </label>
+                    <input className={style.loginInputs} id='email' placeholder='Ivan@gmail.com' name='email'></input>
+                </div>
+                <div className={style.inputDiv}>
+                    <label className={style.loginLabels} htmlFor='password'>
+                        password
+                    </label>
+                    <input
+                        className={style.loginInputs}
+                        id='password'
+                        placeholder='***********'
+                        name='password'
+                        type='password'></input>
+                </div>
                 <button className={style.logBtn}>Login</button>
-                <p>
-                    If you dont have register <a>click here</a>{" "}
+                <p className={style.registerText}>
+                    If you don't have a registration, click{" "}
+                    <a className={style.registerLink} href='/register'>
+                        {" "}
+                        here
+                    </a>{" "}
                 </p>
             </form>
+            <img className={style.img} src='./login-page-asset.svg' alt='login page asset'></img>
         </section>
     );
 }
