23a3f8c263376a0eabe21ecb6ed68533e0a93405
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-25 21:09:31 +0300

    implement new design for the createEvent component

--- src/components/CreateEvent/CreateEvent.js
+++ src/components/CreateEvent/CreateEvent.js
@@ -1,26 +1,25 @@
-import style from "./CreateEvent.module.css";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-
+import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
+import { AuthContext } from "../../context/authContext";
 import * as requester from "../../service/requester";
 import * as riddleService from "../../service/riddleService";
 import Checkout from "../Checkout/Checkout";
 
+import style from "./CreateEvent.module.css";
+
 export default function CreateEvent() {
-    // let [guessedQuestions, setGuessedQuestions] = useState(0)
-    // let [questions, setQuestions] = useState("");
-    // let [answer, setAnswer] = useState("");
-    // let [end, setEnd] = useState(false);
-    // let [endMsg, setEndMsg] = useState("");
-    // let [url, setUrl] = useState("");
+    const { user } = useContext(AuthContext);
+
     const [res, setRes] = useState({});
     const [loadPayment, setLoadPayment] = useState(false);
     const [discountAmount, setDiscountAmount] = useState(0);
     const [discount_code, setDiscountCode] = useState("");
+    const [discountInput, setDiscountInput] = useState("");
 
-    let { riddleId } = useParams();
+    const { riddleId } = useParams();
 
     const navigate = useNavigate();
 
@@ -29,20 +28,7 @@ export default function CreateEvent() {
             let response = await riddleService.getOne(riddleId);
             setRes(response);
         };
-        // const fetchData = async () => {
-        //     let response = await riddleService.createEvent(riddleId)
 
-        //     let firstHalf = response.url.slice(0, 34)
-        //     let token = response.url.slice(34)
-
-        //     if(firstHalf=="http://localhost:3000/event?token=" && token){
-        //         navigate(`/event?token=${token}`)
-        //     }else{
-        //         console.log(response);
-        //     }
-
-        // }
-        // fetchData().then(err=> console.log(err))
         fetchRiddleInfo().then((err) => console.log(err));
     }, []);
 
@@ -52,31 +38,48 @@ export default function CreateEvent() {
         setLoadPayment(true);
     };
 
+    const discountValidation = () => {
+        const code = { discount_code: discountInput };
+        console.log(code);
+
+        requester.post("http://127.0.0.1:5000/discount/validate", JSON.stringify(code)).then((resp) => {
+            console.log("dsadsa", res, "discount_code response");
+            if (resp["is_valid"]) {
+                console.log(resp.discount);
+                setDiscountAmount(Number(resp.discount));
+                // let newPrice = res.price - (res.price*Number(resp.discount)/100)
+                // setRes({...res, price: newPrice})
+                setLoadPayment(false);
+                setDiscountCode(discount_code);
+            } else {
+                setDiscountAmount(0);
+                setDiscountCode("");
+                setLoadPayment(false);
+                alert("Discount code is invalid!");
+            }
+        });
+    };
+
     const onSubmit = (e) => {
         e.preventDefault();
         const discount_code = Object.fromEntries(new FormData(e.target));
         console.log(discount_code);
-        requester
-            .post(
-                "http://127.0.0.1:5000/discount/validate",
-                JSON.stringify(discount_code)
-            )
-            .then((resp) => {
-                // console.log(res, "discount_code response")
-                if (resp["is_valid"]) {
-                    console.log(resp.discount);
-                    setDiscountAmount(Number(resp.discount));
-                    // let newPrice = res.price - (res.price*Number(resp.discount)/100)
-                    // setRes({...res, price: newPrice})
-                    setLoadPayment(false);
-                    setDiscountCode(discount_code);
-                } else {
-                    setDiscountAmount(0);
-                    setDiscountCode("");
-                    setLoadPayment(false);
-                    alert("Discount code is invalid!");
-                }
-            });
+        requester.post("http://127.0.0.1:5000/discount/validate", JSON.stringify(discount_code)).then((resp) => {
+            // console.log(res, "discount_code response")
+            if (resp["is_valid"]) {
+                console.log(resp.discount);
+                setDiscountAmount(Number(resp.discount));
+                // let newPrice = res.price - (res.price*Number(resp.discount)/100)
+                // setRes({...res, price: newPrice})
+                setLoadPayment(false);
+                setDiscountCode(discount_code);
+            } else {
+                setDiscountAmount(0);
+                setDiscountCode("");
+                setLoadPayment(false);
+                alert("Discount code is invalid!");
+            }
+        });
     };
 
     return (
@@ -84,122 +87,101 @@ export default function CreateEvent() {
             <section className={style.eventWhapper}>
                 <PayPalScriptProvider
                     options={{
-                        "client-id":
-                            "AWfYTOnjSJgtSZaqRdR1SjIkehKuXp8GSWXGP3-K1udlWgq64mOv9znAyXa7EyLANzSmkJ-y7myqX0J8",
+                        "client-id": "AWfYTOnjSJgtSZaqRdR1SjIkehKuXp8GSWXGP3-K1udlWgq64mOv9znAyXa7EyLANzSmkJ-y7myqX0J8",
                         currency: "EUR",
-                    }}
-                >
-                    <div className={style.product}>
-                        <div className={style.checkoutInfo}>
-                            <h1 className={style.title}>
-                                Checkout for {res.title}
-                            </h1>
-                            <iframe
-                                src={res.google_map}
-                                className={style.googleMap}
-                            ></iframe>
-                            <form
-                                onSubmit={onSubmit}
-                                className={style.discountForm}
-                            >
-                                <label htmlFor="discount"></label>
-                                <input
-                                    type="text"
-                                    id="discount_code"
-                                    name="discount_code"
-                                    className={style.discountFormInput}
-                                />
-                                <button>Check</button>
-                                {discount_code !== "" ? (
-                                    <p
-                                        className={style.discountCode}
-                                    >{`Dscount code: ${discount_code.discount_code} for ${discountAmount}% off`}</p>
-                                ) : null}
-                            </form>
-                            <div className={style.groupPriceDiv}>
-                                <div className={style.priceInfoDiv}>
-                                    <p>{res.title}</p>
-                                    <p>
-                                        $
-                                        {Number.parseFloat(res.price).toFixed(
-                                            2
+                    }}>
+                    <div className={style.wrapper}>
+                        <h2 className={style.title}>Checkout for {res.title}</h2>
+                        <div className={style.map}>
+                            <iframe title={res.google_map} src={res.google_map} className={style.googleMap}></iframe>
+                        </div>
+                        <div className={style.info}>
+                            <div className={style.infoTitleDiv}>
+                                <p>Purchase details</p>
+                            </div>
+                            <div className={style.description}>
+                                <p className={style.desc}>Description: {res.description}</p>
+                                <p className={style.descP}>Number of questions: {res.number_of_questions}</p>
+                                <p className={style.descP}>Duration: {res.duration}</p>
+                                <p className={style.descP}>Location: {res.where}</p>
+                            </div>
+                        </div>
+                        <div className={style.summary}>
+                            <div className={style.inputsDiv}>
+                                <div className={style.inputDiv}>
+                                    <label className={style.label} htmlFor='email'>
+                                        email address
+                                    </label>
+                                    <input
+                                        disabled
+                                        className={style.input}
+                                        value={user.email}
+                                        placeholder='Admin@admin.com'
+                                        name='email'></input>
+                                </div>
+                                <div className={style.inputDiv}>
+                                    <label
+                                        className={`${style.label} ${discountAmount !== 0 ? style.validLabel : ""}`}
+                                        htmlFor='discount'>
+                                        discount code
+                                    </label>
+                                    <input
+                                        className={`${style.input} ${discountAmount !== 0 ? style.validInput : ""}`}
+                                        value={discountInput}
+                                        onChange={(e) => setDiscountInput(e.target.value)}
+                                        placeholder='code'
+                                        name='discount'></input>
+                                    <div className={discountAmount !== 0 ? style.textSpaceBetween : style.textRight}>
+                                        {discountAmount !== 0 ? (
+                                            <p className={style.greenText}>{discountAmount}% off!</p>
+                                        ) : (
+                                            ""
                                         )}
-                                    </p>
-                                </div>
-                                <div className={style.priceInfoDiv}>
-                                    <p>Discount</p>
-                                    <p>
-                                        $
-                                        {Number.parseFloat(
-                                            (res.price * discountAmount) / 100
-                                        ).toFixed(2)}
-                                    </p>
-                                </div>
-                                <div className={style.priceInfoDiv}>
-                                    <p className={style.bold}>All</p>
-                                    <p className={style.bold}>
-                                        $
-                                        {Number.parseFloat(
-                                            res.price -
-                                                (res.price * discountAmount) /
-                                                    100
-                                        ).toFixed(2)}
-                                    </p>
+                                        <p
+                                            onClick={() => {
+                                                discountValidation();
+                                            }}
+                                            className={style.checkCode}>
+                                            Check discount code
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={style.horizontalDevider}> </div>
-
-                            <div className={style.paypalBtn}>
-                                {!loadPayment ? (
-                                    <button
-                                        className={style.loadPaymentBtn}
-                                        onClick={onClick}
-                                    >
-                                        Load Payment options
-                                    </button>
-                                ) : (
-                                    <Checkout
-                                        riddle={res}
-                                        discount={discountAmount}
-                                    />
-                                )}
+                            <div className={style.pricing}>
+                                <div className={style.infoTitleDiv}>
+                                    <p>Pricing</p>
+                                </div>
+                                <div className={style.priceSummary}>
+                                    <div className={style.row}>
+                                        <p>Price</p>
+                                        <p>${Number.parseFloat(res.price).toFixed(2)}</p>
+                                    </div>
+                                    <div className={style.row}>
+                                        <p>Discount</p>
+                                        <p>${Number.parseFloat((res.price * discountAmount) / 100).toFixed(2)}</p>
+                                    </div>
+                                    <div className={style.horizontalDivider}></div>
+                                    <div className={style.row}>
+                                        <p>Total</p>
+                                        <p>
+                                            $
+                                            {Number.parseFloat(res.price - (res.price * discountAmount) / 100).toFixed(
+                                                2
+                                            )}
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div className={style.riddleInfo}>
-                            <p className={style.riddleInfoP}>Description: {res.description}</p>
-                            <p className={style.riddleInfoP}>
-                                Number of questions: {res.number_of_questions}
-                            </p>
-                            <p className={style.riddleInfoP}>Duration: {res.duration}</p>
-                            <p className={style.riddleInfoP}>Where: {res.where}</p>
+                        <div className={style.paypalBtn}>
+                            {!loadPayment ? (
+                                <button className={style.loadPaymentBtn} onClick={onClick}>
+                                    Load Payment options
+                                </button>
+                            ) : (
+                                <Checkout riddle={res} discount={discountAmount} />
+                            )}
                         </div>
-                        {/* <p>Title: {res.title}</p>
-                <p>Discount: {res.discount}</p>
-                {discountAmount !== 0 ? <p>Price after discount: {res.price-(res.price*discountAmount/100)}</p>:<p>Price: {res.price}</p>}
-                <p>Id: {res.id}</p>
-                <p>Description: {res.description}</p>
-                <p>Number_of_questions: {res.number_of_questions}</p>
-                <p>Duration: {res.duration}</p>
-                <p>Where: {res.where}</p>
-                <iframe src={res.google_map} width="640" height="480"></iframe>
-                <form onSubmit={onSubmit}>
-                    <label htmlFor="discount"></label>
-                    <input
-                        type="text"
-                        id="discount_code"
-                        name="discount_code"
-                    />
-                    <button>Check</button>
-                    {discount_code !== "" ?<p>{`Dscount code: ${discount_code.discount_code} for ${discountAmount}% off`}</p>: null}
-                </form>
-                
-
-                {!loadPayment ? (
-                    <button onClick={onClick}>Load Payment options</button>
-                ) : (
-                    <Checkout riddle={res} discount={discountAmount} />
-                )} */}
                     </div>
                 </PayPalScriptProvider>
             </section>
