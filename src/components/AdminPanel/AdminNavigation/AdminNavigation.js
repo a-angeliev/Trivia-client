ed397fe9f92e5d2bfd3410274b9aa6fd5aa733f7
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-26 11:46:02 +0300

    fix typo in admin navigation

--- src/components/AdminPanel/AdminNavigation/AdminNavigation.js
+++ src/components/AdminPanel/AdminNavigation/AdminNavigation.js
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
-import style from './AdminNavigation.module.css'
+import style from "./AdminNavigation.module.css";
 export default function AdminNavigation() {
     return (
         <div className={style.AdminNavDiv}>
+            <ul className={style.AdminNavUl}>
+                <li className={style.AdminNavLi}>
+                    <Link className={style.AdminNavLink} to={"/admin-panel"}>
+                        riddles
+                    </Link>
+                </li>
+                <div className={style.devider}></div>
+                <li className={style.AdminNavLi}>
+                    <Link className={style.AdminNavLink} to={"/admin-panel/discount"}>
+                        discounts
+                    </Link>
+                </li>
+                <div className={style.devider}></div>
 
-            <ul  className={style.AdminNavUl}>
                 <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel"}>Riddles</Link>
-                </li>
-                <div className={style.devider}></div>
-                <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel/discount"}>Discounts</Link>
-                </li>
-                <div className={style.devider}></div>
-        
-                <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel/transactions"}>Transactions</Link>
+                    <Link className={style.AdminNavLink} to={"/admin-panel/transactions"}>
+                        transactions
+                    </Link>
                 </li>
             </ul>
         </div>
