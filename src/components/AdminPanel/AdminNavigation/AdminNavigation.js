f6592937ed5d6fd2981468d44c2b23d7547edc99
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-26 11:52:05 +0300

    change class names in adminNavigation

--- src/components/AdminPanel/AdminNavigation/AdminNavigation.js
+++ src/components/AdminPanel/AdminNavigation/AdminNavigation.js
@@ -2,23 +2,23 @@ import { Link } from "react-router-dom";
 import style from "./AdminNavigation.module.css";
 export default function AdminNavigation() {
     return (
-        <div className={style.AdminNavDiv}>
-            <ul className={style.AdminNavUl}>
-                <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel"}>
+        <div className={style.adminNavDiv}>
+            <ul className={style.adminNavUl}>
+                <li className={style.adminNavLi}>
+                    <Link className={style.adminNavLink} to={"/admin-panel"}>
                         riddles
                     </Link>
                 </li>
-                <div className={style.devider}></div>
-                <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel/discount"}>
+                <div className={style.divider}></div>
+                <li className={style.adminNavLi}>
+                    <Link className={style.adminNavLink} to={"/admin-panel/discount"}>
                         discounts
                     </Link>
                 </li>
-                <div className={style.devider}></div>
+                <div className={style.divider}></div>
 
-                <li className={style.AdminNavLi}>
-                    <Link className={style.AdminNavLink} to={"/admin-panel/transactions"}>
+                <li className={style.adminNavLi}>
+                    <Link className={style.adminNavLink} to={"/admin-panel/transactions"}>
                         transactions
                     </Link>
                 </li>
