cc2565f185e1e333216e3a3d0384d40986101a99
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-30 15:29:12 +0200

    implement new design for the DiscplayEventStatCheck component

--- src/components/DisplayEvent/DIsplayEventStartCheck/DisplayEventStartCheck.js
+++ src/components/DisplayEvent/DIsplayEventStartCheck/DisplayEventStartCheck.js
@@ -1,28 +1,36 @@
-import * as requester from "../../../service/requester";
 import { useNavigate } from "react-router-dom";
-import style from "./DisplayEventStartCheck.module.css"
+
+import * as requester from "../../../service/requester";
+
+import style from "./DisplayEventStartCheck.module.css";
 
 export default function DisplayEventStartCheck(props) {
     let navigate = useNavigate();
-    const onClick = (e) => {
-        if(window.confirm("Are you sure you want to start ?")){
 
+    const onClick = (e) => {
+        if (window.confirm("Are you sure you want to start ?")) {
             requester
-            .post(`http://127.0.0.1:5000/event?token=${props.token}`, {
-                "": "",
-            })
-            .then((res) => {
-                props.setRes(res);
-                navigate(`?token=${props.token}`);
-            });
+                .post(`http://127.0.0.1:5000/event?token=${props.token}`, {
+                    "": "",
+                })
+                .then((res) => {
+                    props.setRes(res);
+                    navigate(`?token=${props.token}`);
+                });
         }
     };
 
     return (
         <>
-            <div className = {style.startActionWhrapper}>
+            <div className={style.startActionWrapper}>
+                <h1 className={style.title}>You are about to start the quiz</h1>
                 <p className={style.startActionP}>{props.massage}</p>
-                <button onClick={onClick}>START</button>
+                <div className={style.btnDiv}>
+                    <button className={style.btn} onClick={onClick}>
+                        start
+                    </button>
+                    <button className={style.btn}>go back</button>
+                </div>
             </div>
         </>
     );
