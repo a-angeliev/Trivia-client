5664d4d02250315f05ebc55ca8c26918f5060c33
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-10-31 17:40:16 +0200

    change variables from let to const in Logout.js

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,10 +1,11 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { AuthContext } from "../../context/authContext";
 
 export default function Logout() {
-    let navigate = useNavigate();
-    let { user, userLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { userLogout } = useContext(AuthContext);
 
     useEffect(() => {
         if (window.confirm("Are you sure you want to logout") == true) {
