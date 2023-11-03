7db7d7da494d5bbee256940f9af31f5f21795e9f
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-11-03 13:15:14 +0200

    implement responsive design for navigation bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 
 import { AuthContext } from "../../context/authContext";
 
@@ -7,6 +7,13 @@ import style from "./Header.module.css";
 
 const Header = () => {
     const { user, isAdmin } = useContext(AuthContext);
+
+    const [toggleChecked, setToggleChecked] = useState(false);
+
+    const location = useLocation();
+    useEffect(() => {
+        setToggleChecked(false);
+    }, [location.pathname]);
 
     return (
         <header>
@@ -17,7 +24,63 @@ const Header = () => {
                     </Link>
                 </div>
 
-                <ul className={style.ulNav}>
+                <div className={`${style.hamburgerMenu} ${style.mobile}`}>
+                    <input
+                        id='menuToggle'
+                        className={style.menuToggle}
+                        checked={toggleChecked}
+                        onChange={() => setToggleChecked((prev) => !prev)}
+                        type='checkbox'
+                    />
+                    <label className={`${style.menuBtn}`} htmlFor='menuToggle'>
+                        <span></span>
+                    </label>
+
+                    <ul className={style.menuBox}>
+                        <li>
+                            <Link className={style.menuItem} to='/about'>
+                                about
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={style.menuItem} to='/riddles'>
+                                riddles
+                            </Link>
+                        </li>
+
+                        {!user.token ? (
+                            <>
+                                <li>
+                                    <Link className={style.menuItem} to='/login'>
+                                        login
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className={style.menuItem} to='/register'>
+                                        register
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                {isAdmin ? (
+                                    <li>
+                                        <Link className={style.menuItem} to='/admin-panel'>
+                                            admin panel
+                                        </Link>
+                                    </li>
+                                ) : null}
+                                <li>
+                                    <Link className={style.menuItem} to='/logout'>
+                                        logout
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+
+                <ul className={`${style.desktop} ${style.ulNav}`}>
                     <li className={style.liNav}>
                         <Link className={style.navLink} to='/about'>
                             about
